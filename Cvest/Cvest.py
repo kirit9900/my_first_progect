b87caa6d1040a42f863badf2f123a129b7e3cd76
kirto <kir.tomskikh@gmail.com>
2024-01-23 20:10:37 +0300

    initial commit

--- Cvest/Cvest.py
+++ Cvest/Cvest.py
@@ -16,7 +16,7 @@ def handler(message):
     data[message.chat.username] = {"WinorFalse": ""}
     save_data(data, data_path)
     bot.send_message(message.chat.id, """Привет мой друг,ты попал на мой квест хоррор по смешарикам!
-    Ты можешь посмотреть информацию в 
+    Ты можешь посмотреть информацию в /help
     """)
     cvestick(message)
 
@@ -27,13 +27,19 @@ def cvestick(message):
     btn2 = types.KeyboardButton("К лосяшу")
     btn3 = types.KeyboardButton("К ежику")
     markup.add(btn1, btn2, btn3)
-    msg = bot.send_message(message.chat.id, """
+    bot.send_message(message.chat.id, """
     Был темный вечер.Тихо ехал поезд.Я посмотрел в окно и увидел густой туман"
     Эх...Надеюсь меня встретят и засел дальше воткнул наушники в уши где играла музыка!
-    Он не заметил как пролетел час и я приехали!
+    Он не заметил как пролетел час и я приехали!""")
+    audio = open(r'C:\Основные проекты\Chipi.mp3', 'rb')
+    bot.send_audio(message.from_user.id, audio)
+    bot.send_photo(message.chat.id, "C:\Основные проекты\my_first_progect\Cvest\kandinsky-download-1706028798257.png")
+    msg =bot.send_message(message.chat.id,"""
     Я с радостью выбегаю и сразу впадаю вступор.
     -Когда я садился в поезд Крош ему сказал что встретит меня...Но его нету!
-    Оставалось самому идти!Но куда?""", reply_markup=markup)
+    Оставалось самому идти!Но куда?""",reply_markup=markup)
+    audio = open(r'C:\Основные проекты\Chipi.mp3', 'rb')
+    bot.send_audio(message.from_user.id, audio)
     bot.register_next_step_handler(msg, opredel)
 
 
@@ -43,13 +49,22 @@ def opredel(message):
     btn2 = types.KeyboardButton("К лосяшу")
     btn3 = types.KeyboardButton("К ежику")
     markup.add(btn1, btn2, btn3)
-    if message.text == "К нюше":
-        nusha(message)
-    elif message.text=="К лосяшу":
-        Loshay(message)
-    elif message.text=="К ежику":
-        eshik(message)
+    while message.text!="К нюше" or "К лосяшу" or "К ежику":
+        if message.text == "К нюше":
+            nusha(message)
+        elif message.text=="К лосяшу":
+            Loshay(message)
+        elif message.text=="К ежику":
+            eshik(message)
+        else:
+            msg = bot.send_message(message.chat.id, "Дорогой друг выбери кнопки!")
+            bot.register_next_step_handler(msg, opredel)
 def eshik(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Подождать")
+    btn2 = types.KeyboardButton("Через окно,но домой")
+    btn3 = types.KeyboardButton("К совунье")
+    markup.add(btn1, btn2, btn3)
     bot.send_message(message.caht.id,"""
     Я подошел к дому Ежика,но свет не был включен!
     Или его нету дома или он спит-додумав решил покричать
@@ -60,7 +75,57 @@ def eshik(message):
     Заходя домой я увидел что все стены были разорваны цветы лежали на полу...
     Ежик....ты где...
     Тут же на тумбочке я нашел его дневник...
-    Я решил чуть чуть прочитать его""")
+    Я решил чуть чуть прочитать его:""")
+    bot.send_message(message.chat.id,Consid.dnevnik)
+    msg=bot.send_message(message.chat.id,"""
+    прочитав все это я не знал что делать...
+    Я сейчас могу попежать на станцию или подождать ежика вдруг все обошлось и он просто ушел куда то или пойти к совунья...
+    Но не успев подумав я очень близко услышал шаги и какой то разряренный голос...""")
+    bot.register_next_step_handler(msg,Vab)
+def Vab(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Подождать")
+    btn2 = types.KeyboardButton("Через окно,но домой")
+    btn3 = types.KeyboardButton("К совунье")
+    markup.add(btn1, btn2, btn3)
+    while message.text != "Подождать" or "Через окно,но домой" or "К совунье":
+        if message.text == "Подождать":
+            Smert(message)
+        elif message.text=="Через окно,но домой":
+            Home(message)
+        elif message.text=="К совунье":
+            sov(message)
+        else:
+            msg = bot.send_message(message.chat.id, "Дорогой друг выбери кнопки!")
+            bot.register_next_step_handler(msg, Vab)
+def Smert(message):
+    bot.send_message(message.chat.id,"Я решил его подождать!")
+    bot.send_message(message.chat.id,"""
+    Через 10 минут он пришел
+    Но это был не ежик!Это был какой то монстр!
+    Он имел красные глаза!
+    Вмговенье как он меня увидел он подошел и укусил!
+    Я потерял сознан....
+                            <GAME OVER>""")
+    user_data[message.chat.username]["WinorFalse"]=False
+def Home(message):
+    bot.send_message(message.chat.id,"""
+    Я решил не испытывать свою судьбу и поэтому...
+    Я быстро и тихо открыл окно и вылез через него
+     Когда я вылез я услышал как открылась дверь и я со всех ног побежал на станцию
+     Я забрался в поезд но машиниста небыло 
+     Проклятье...
+     Но зяйдя в рубку я понял что он был на автопилоте
+     Я быстро разобрался и поезд поехал
+     Фух....
+                    <Конец>""")
+    user_data[message.chat.username]["WinorFalse"] =True
+def sov(message):
+    bot.send_message(message.chat.id, """
+        Я решил не испытывать свою судьбу и поэтому...
+        Я быстро и тихо открыл окно и вылез через него
+        Когда я вылез я услышал как открылась дверь и я со всех ног побежал к совунье""")
+    sovunia(message)
 
 def Loshay(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -85,26 +150,32 @@ def Loshay(message):
     НЕ надо чуть ли не закричал Лосяш
     Давай подождем и тихо через окно уйдем только я винтовку возьму!
     Что делать тебе?
-    """)
+    """,reply_markup=markup)
+    Losh(message)
 def Losh(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("Ждать")
     btn2 = types.KeyboardButton("Открыть")
     btn3 = types.KeyboardButton("Через окно")
     markup.add(btn1, btn2, btn3)
-    if message.text=="Ждать":
-        msg=bot.send_message("""Мы ждем 20 минут и стуки стихают!"
-                    Мы выходим и бежим со всех ног к совунье""")
-        bot.register_next_step_handler(msg, sovunia2)
-    elif message.text=="Открыть":
-        bot.send_message("""Ты окрываешь дверь и на тебя налетает монстр ежик и кусает тебя"
-                         Через 10 минут становишься таким же зомби!
-                                        <Game Over>""")
-    elif message.text=="Через окно":
-        msg = bot.send_message("""Мы решили через окно!
-        Мы тихо его открываем и вылезаем тихо тихо
-        Отходим пару метров и бежим со всех ног к совунье!""")
-        bot.register_next_step_handler(msg, sovunia2)
+    while message.text != "Ждать" or "Через окно" or "открыть":
+        if message.text=="Ждать":
+            msg=bot.send_message("""Мы ждем 20 минут и стуки стихают!"
+                        Мы выходим и бежим со всех ног к совунье""")
+            bot.register_next_step_handler(msg, sovunia2)
+        elif message.text=="Открыть":
+            bot.send_message("""Ты окрываешь дверь и на тебя налетает монстр ежик и кусает тебя"
+                             Через 10 минут становишься таким же зомби!
+                                            <Game Over>""")
+            user_data[message.chat.username]["WinorFalse"] = False
+        elif message.text=="Через окно":
+            msg = bot.send_message("""Мы решили через окно!
+            Мы тихо его открываем и вылезаем тихо тихо
+            Отходим пару метров и бежим со всех ног к совунье!""")
+            bot.register_next_step_handler(msg, sovunia2)
+        else:
+            msg = bot.send_message(message.chat.id, "Дорогой друг выбери кнопки!")
+            bot.register_next_step_handler(msg, Losh)
 def sovunia2(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("1")
@@ -162,11 +233,14 @@ def handle_buttons(message):
     btn1 = types.KeyboardButton("1")
     btn2 = types.KeyboardButton("2")
     markup.add(btn1, btn2)
-    if message.text == "1":
-        pin2(message)
-    elif message.text == "2":
-        gameover(message)
-
+    while message.text != "1" or message.text != "2":
+        if message.text == "1":
+            pin2(message)
+        elif message.text == "2":
+            gameover(message)
+        else:
+            msg = bot.send_message(message.chat.id,"Дорогой друг выбери кнопки!")
+            bot.register_next_step_handler(msg, handle_buttons)
 
 def pin2(message):
     bot.send_message(message.chat.id,
@@ -199,6 +273,7 @@ def gameover(message):
     Тут я вылетаю на улицу и понимаю что на руке у меня укус\n
     Через 10 минут я ничего не понимаю и тут я бросаюсь на других...!""")
     bot.send_message(message.chat.id, "Game over")
+    user_data[message.chat.username]["WinorFalse"] = False
 
 
 def pin(message):
@@ -249,10 +324,14 @@ def lodka(message):
     btn1 = types.KeyboardButton("В красную")
     btn2 = types.KeyboardButton("В белую")
     markup.add(btn1, btn2)
-    if message.text == "В белую":
-        bad(message)
-    elif message.text == "В красную":
-        Good(message)
+    while message.text != "В белую" or message.text !="В красную":
+        if message.text == "В белую":
+            bad(message)
+        elif message.text == "В красную":
+            Good(message)
+        else:
+            msg = bot.send_message(message.chat.id, "Дорогой друг выбери кнопки!")
+            bot.register_next_step_handler(msg, lodka)
 
 
 def Good(message):
@@ -261,7 +340,7 @@ def Good(message):
     Через 5 минут к ним подплыла лодка и забрала всех!
     Хороший конец но дальше больше интересного но это будет другая история!
     """)
-    exit()
+    user_data[message.chat.username]["WinorFalse"] = True
 
 
 def bad(message):
@@ -296,21 +375,26 @@ def puti(message):
     btn2 = types.KeyboardButton("Через дом совуньи")
     btn3 = types.KeyboardButton("Остаться")
     markup.add(btn1, btn2, btn3)
-    if message.text == "Через дом кроша":
-        Crosh(message)
-    elif message.text == "Через дом Совуньи":
-        Sovusha(message)
-    else:
-        bot.send_message(message.chat.id, """
-        Мы пошли к пину и проседели там 5 часов
-        Потом мы услышали что очень близко были шаги очень громкие!
-        Мы решили закрыться и подумали что они скоро уйдут
-        Да не тут это было!
-        Нас дерзжали в заперти примерно 5 суток без еды!
-        Так что мы умерли!
-        """)
-        bot.send_message(message.chat.id, """
-                                <GAME OVER>""")
+    while message.text != "Через дом кроша" or message.text !="Через дом Совуньи":
+        if message.text == "Через дом кроша":
+            Crosh(message)
+        elif message.text == "Через дом Совуньи":
+            Sovusha(message)
+        elif message.text == "Остаться":
+            bot.send_message(message.chat.id, """
+            Мы пошли к пину и проседели там 5 часов
+            Потом мы услышали что очень близко были шаги очень громкие!
+            Мы решили закрыться и подумали что они скоро уйдут
+            Да не тут это было!
+            Нас дерзжали в заперти примерно 5 суток без еды!
+            Так что мы умерли!
+            """)
+            bot.send_message(message.chat.id, """
+                                    <GAME OVER>""")
+            user_data[message.chat.username]["WinorFalse"] = False
+        else:
+            msg = bot.send_message(message.chat.id, "Дорогой друг выбери кнопки!")
+            bot.register_next_step_handler(msg, puti)
 
 
 def Crosh(message):
@@ -337,19 +421,23 @@ def Crosh(message):
 
 def Boss(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("По тселсу")
+    btn1 = types.KeyboardButton("По стелсу")
     btn2 = types.KeyboardButton("Отвлечь")
     btn3 = types.KeyboardButton("Пролом")
     btn4 = types.KeyboardButton("Убить")
     markup.add(btn1, btn2, btn3, btn4)
-    if message.text == "По тселсу":
-        stelc(message)
-    elif message.text == "Отвлечь":
-        Otvlech(message)
-    elif message.text == "prolom":
-        Prolom(message)
-    elif message.text=="Убить":
-        Bay(message)
+    while message.text != "По стелсу" or message.text !="Отвлечь" or message.text !="prolom" or message.text !="Убить":
+        if message.text == "По стелсу":
+            stelc(message)
+        elif message.text == "Отвлечь":
+            Otvlech(message)
+        elif message.text == "prolom":
+            Prolom(message)
+        elif message.text=="Убить":
+            Bay(message)
+        else:
+            msg=bot.send_message(message.chat.id,"Дорогой друг выбери кнопки!")
+            bot.register_next_step_handler(msg, Boss)
 def stelc(message):
     a = random.randint(10, 100)
     b = random.randint(1, 8)
@@ -361,6 +449,7 @@ def stelc(message):
                              Мы уехали,но дальше будет еще интерестнее!
                               Ждите обнов!
                                 Конец!\n""")
+        user_data[message.chat.username]["WinorFalse"] = True
     elif a < 60:
         bot.send_message(message.chat.id, f"""
             Я и несколько товарищей смогли пройти сквозь монстров,но {b} товарищей не смогли пройти,
@@ -370,6 +459,7 @@ def stelc(message):
             Мы уехали,но дальше будет еще интерестнее!
             Ждите обнов
                     Конец!\n""")
+        user_data[message.chat.username]["WinorFalse"] = True
 def Otvlech(message):
     a = random.randint(10, 100)
     b = random.randint(1, 8)
@@ -383,6 +473,7 @@ def Otvlech(message):
                              Мы уехали,но дальше будет еще интерестнее!
                               Ждите обнов!
                               Конец!\n""")
+        user_data[message.chat.username]["WinorFalse"] = True
     elif a < 40:
             bot.send_message(message.chat.id, f"""
             Я и несколько товарищей смогли отвлечь,но {b} товарищей не смогли пройти,
@@ -392,6 +483,7 @@ def Otvlech(message):
             Мы уехали,но дальше будет еще интерестнее!
             Ждите обнов,написано на главном экране поезда!
                                 Конец\n""")
+    user_data[message.chat.username]["WinorFalse"] = True
 def Prolom(message):
     a = random.randint(10, 100)
     b = random.randint(1, 8)
@@ -405,6 +497,7 @@ def Prolom(message):
                              Мы уехали,но дальше будет еще интерестнее!
                               Ждите обнов!
                               Конец\n""")
+        user_data[message.chat.username]["WinorFalse"] = True
     elif a < 40 and 9-c!=0 and 9-c!=1:
             bot.send_message(message.chat.id, f"""
             Я и несколько товарищей смогли отвлечь сквозь монстров,но {c} товарищей не смогли пройти,
@@ -418,12 +511,14 @@ def Prolom(message):
             bot.send_message(message.chat.id, f"""
             Мы не смогли пройти на пролом нас всех сьели!
                             <GAME OVER>""")
+            user_data[message.chat.username]["WinorFalse"] = False
     elif a < 40 and 9-c==1:
             bot.send_message(message.chat.id, """
             Я остался один
             Я со всех ног бегу на поезд
             Мой андруналин помог мне разобраться с поездом и я уехал от сюда!
             Ждите обнов""")
+    user_data[message.chat.username]["WinorFalse"] = True
 def Bay(message):
     a = random.randint(10, 100)
     b = random.randint(1, 8)
@@ -439,6 +534,7 @@ def Bay(message):
                              Мы уехали,но дальше будет еще интерестнее!
                               Ждите обнов!
                               Конец\n""")
+        user_data[message.chat.username]["WinorFalse"] = True
     elif a < 40 and 4-d!=0 and 4-d!=1:
             bot.send_message(message.chat.id, f"""
             Мы со всеми дошли до дома Кар-Карыча,взяли оружие и я лосяш,бараш и копатыч пошли их убивать!,но {d} товарищей не смогли пройти,
@@ -448,6 +544,7 @@ def Bay(message):
             Мы уехали,но дальше будет еще интерестнее!
             Ждите обнов,написано на главном экране поезда!
                             Конец\n""")
+            user_data[message.chat.username]["WinorFalse"] = True
     elif a < 40 and 4-d==1:
             bot.send_message(message.chat.id, """
             Я остался один,убив кроша я бегу в дом и Кричу Быстро к поезду!
@@ -459,6 +556,7 @@ def Bay(message):
             Мы не смогли убить их
             Нас всех сьели!
                             <GAME OVER>\n""")
+    user_data[message.chat.username]["WinorFalse"] = False
 
 
 def Sovusha(message):
@@ -471,6 +569,7 @@ def Sovusha(message):
     Мы уехали,но дальше будет еще интерестнее!
     Ждите обнов,написано на главном экране поезда!
                             Конец\n""")
+    user_data[message.chat.username]["WinorFalse"] = True
 
 
 bot.polling()
